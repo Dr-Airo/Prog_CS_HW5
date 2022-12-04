c5f2d8a3e58559befbcbcf2803d4992d18aba630
Dr-Airo <nurtdinov_ar@mail.ru>
2022-12-04 22:03:10 +0300

    HW 1

---
+++ 3. Programms (Python)/sem1/HW1.py
@@ -0,0 +1,39 @@
+
+# №1
+#a = int(input ("Введите день недели: "))
+#if a == 6 or a == 7: print ("Это выходной")
+#else: print ("Это будний день")
+
+# №2
+#for x in range(4):
+#        for y in range(4):
+#            for z in range(4):
+#                print(not (x or y or z) == (not x and not y and not z))
+#                print(x, y, z)
+
+# №3
+#x = int(input ("Введите координату по X: "))
+#y = int(input ("Введите координату по Y: "))
+#if x>0 and y>0: print ("Это первая четверть")
+#if x>0 and y<0: print ("Это вторая четверть")
+#if x<0 and y>0: print ("Это третья четверть")
+#if x<0 and y<0: print ("Это четвертая четверть")
+
+# №4
+#a = int(input ("Введите номер четверти: "))
+#if a == 1: print ("x>0, y>0")
+#if a == 2: print ("x>0, y<0")
+#if a == 3: print ("x<0, y>0")
+#if a == 4: print ("x<0, y<0")
+#else:  print ("Введены некорретные данные")
+
+# №5
+#import math
+
+#Ax = float(input('Введите координату точки А по оси Х: '))
+#Ay = float(input('Введите координату точки А по оси Y: '))
+#Bx = float(input('Введите координату точки B по оси Х: '))
+#By = float(input('Введите координату точки B по оси Y: '))
+
+#distance = round(math.sqrt((Bx - Ax)**2 + (By - Ay)**2))
+#print(distance)
